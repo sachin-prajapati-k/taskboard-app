97b582827c2bd608d4b3915aa2f3d86be199436d
sagar <sdev77577@gmail.com>
2026-03-22 21:49:18 +0530

    sdfgsdfg

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="">hello</div>;
+  async function addTask(formData: FormData) {
+    "use server";
+    const Task = formData.get("task")?.toString();
+    console.log(Task);
+  }
+  return (
+    <div className="">
+      <div>
+        <form action={addTask}>
+          <label>Add your Task</label>
+          <input type="text" placeholder="enter your task" name="task" />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+    </div>
+  );
 }
